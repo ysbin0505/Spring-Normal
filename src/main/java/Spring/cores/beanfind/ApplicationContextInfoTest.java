6ab7856237c37d210e189d6bc7a308b18e667204
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-07-07 22:22:25 +0900

    dd

---
+++ src/main/java/Spring/cores/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,11 @@
+package Spring.cores.beanfind;
+
+import Spring.cores.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+  AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+}
