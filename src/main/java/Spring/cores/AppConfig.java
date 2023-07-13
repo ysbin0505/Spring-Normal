7a8cd0a0892432721fa9edc106f8f80da8324841
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-07-13 17:34:03 +0900

    @Configuration과 싱글톤, 바이트코드 조작

--- src/main/java/Spring/cores/AppConfig.java
+++ src/main/java/Spring/cores/AppConfig.java
@@ -17,16 +17,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+  //호출되는 순서
+  //call AppConfig.memberService
+  //call AppConfig.memberRepository
+  //call AppConfig.memberRepository
+  //call AppConfig.orderService
+  //call AppConfig.memberRepository
+
+  //-> 실제 테스트해보면 싱글톤 보장
+  //call AppConfig.memberService
+  //call AppConfig.memberRepository
+  //call AppConfig.orderService
+
   @Bean
   public MemberService memberService(){
+    System.out.println("call AppConfig.memberService");
     return new MemberServiceImpl(memberRepository());
   }
   @Bean
   public MemberRepository memberRepository() {
+    System.out.println("call AppConfig.memberRepository");
     return new MemoryMemberRepository();
   }
   @Bean
   public OrderService orderService(){
+    System.out.println("call AppConfig.orderService");
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
   @Bean
---
+++ src/test/java/Spring/cores/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,42 @@
+package Spring.cores.singleton;
+
+import Spring.cores.AppConfig;
+import Spring.cores.member.MemberRepository;
+import Spring.cores.member.MemberServiceImpl;
+import Spring.cores.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+  @Test
+  void configurationTest(){
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+    OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+    MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+    MemberRepository memberRepository1 = memberService.getMemberRepository();
+    MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+    System.out.println("memberService -> memberRepository = " + memberRepository1);
+    System.out.println("orderService -> memberRepository = " + memberRepository2);
+    System.out.println("memberRepository = " + memberRepository);
+
+    assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+    assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+  }
+
+  @Test
+  void configurationDeep(){
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AppConfig bean = ac.getBean(AppConfig.class);
+
+    System.out.println("bean = " + bean.getClass());
+  }
+}
