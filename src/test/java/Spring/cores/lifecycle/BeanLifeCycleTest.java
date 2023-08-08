12bf4e3f5bb36f5124abbe65b0be062997aebf79
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-08 14:56:33 +0900

    이니셜라이징빈, 디스바저블빈

--- src/test/java/Spring/cores/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/Spring/cores/lifecycle/BeanLifeCycleTest.java
@@ -1,6 +1,7 @@
 package Spring.cores.lifecycle;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -12,19 +13,12 @@ public class BeanLifeCycleTest {
   public void lifeCycleTest(){
     ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
     NetworkClient client = ac.getBean(NetworkClient.class);
-
-    // 외부에서 setUrl() 메서드 호출
-    client.setUrl("http://modified-url.dev");
-
-    // 초기화 작업 메서드 호출
-    client.init();
-
     ac.close();
   }
 
   @Configuration
   static class LifeCycleConfig{
-    @Bean(initMethod = "init") // 초기화 메서드 지정
+    @Bean
     public NetworkClient networkClient(){
       NetworkClient networkClient = new NetworkClient();
       networkClient.setUrl("http://hello-spring.dev");
--- src/test/java/Spring/cores/lifecycle/NetworkClient.java
+++ src/test/java/Spring/cores/lifecycle/NetworkClient.java
@@ -1,21 +1,21 @@
 package Spring.cores.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
+
+  public NetworkClient() {
+    System.out.println("생성자 호출, url = " + url);
+  }
 
   public void setUrl(String url) {
     this.url = url;
   }
 
-  // 초기화 작업을 수행하는 메서드
-  public void init() {
-    System.out.println("객체 초기화 작업 수행, url = " + url);
-    connect();
-    call("초기화 연결 메시지");
-  }
-
-  // 서비스 시작 시 호출
+  //서비스 시작 시 호출
   public void connect(){
     System.out.println("connect : " + url);
   }
@@ -24,8 +24,21 @@ public class NetworkClient {
     System.out.println("call : " + url + " message = " + message);
   }
 
-  // 서비스 종료 시 호출
+  //서비스 종료 시 호출
   public void disconnect(){
     System.out.println("close : " + url);
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    System.out.println("NetworkClient.afterPropertiesSet");
+    connect();
+    call("초가화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    System.out.println("NetworkClient.destroy");
+    disconnect();
   }
 }
