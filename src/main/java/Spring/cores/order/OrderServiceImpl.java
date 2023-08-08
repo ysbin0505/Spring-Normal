c60881dec7712838d435e2f3fe7ee24bdf38d3a8
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-08 09:52:22 +0900

    롬복

--- src/main/java/Spring/cores/order/OrderServiceImpl.java
+++ src/main/java/Spring/cores/order/OrderServiceImpl.java
@@ -41,7 +41,7 @@ public class OrderServiceImpl implements OrderService {
     System.out.println("discountPolicy = " + discountPolicy);
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
-  }zsdv*/
+  }zsdsv*/
 
 
   @Override
