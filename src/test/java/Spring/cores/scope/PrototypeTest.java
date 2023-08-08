51613020da09e2fdc2b5a6a7231a90a44d43cbf6
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-08 15:50:54 +0900

    싱글톤 스코프와 프로토타입 스코프

--- src/test/java/Spring/cores/scope/PrototypeTest.java
+++ src/test/java/Spring/cores/scope/PrototypeTest.java
@@ -23,6 +23,7 @@ public class PrototypeTest {
     System.out.println("prototypeBean2 = " + prototypeBean2);
     assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
 
+    ac.close();
   }
 
     @Scope("prototype")
