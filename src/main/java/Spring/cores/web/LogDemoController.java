3a185c54bc9019d3c33e0cb8f635727487eb0215
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-13 15:43:12 +0900

    log demo

---
+++ src/main/java/Spring/cores/web/LogDemoController.java
@@ -0,0 +1,23 @@
+package Spring.cores.web;
+import Spring.cores.common.MyLogger;
+//import Spring.cores.logdemo.LogDemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+  //private final LogDemoService logDemoService;
+  private final MyLogger myLogger;
+  @RequestMapping("log-demo")
+  @ResponseBody
+  public String logDemo(HttpServletRequest request) {
+    String requestURL = request.getRequestURL().toString();
+    myLogger.setRequestURL(requestURL);
+    myLogger.log("controller test");
+    //logDemoService.logic("testId");
+    return "OK";
+  }
+}
