9c55e7edb87110d6d69829a92646d948376efacf
ysbin0505@naver.com <118337787+ysbin0505@users.noreply.github.com>
2023-08-13 15:47:24 +0900

    Scope와 provider

--- src/main/java/Spring/cores/web/LogDemoController.java
+++ src/main/java/Spring/cores/web/LogDemoController.java
@@ -1,7 +1,8 @@
 package Spring.cores.web;
 import Spring.cores.common.MyLogger;
-//import Spring.cores.logdemo.LogDemoService;
+import Spring.cores.logdemo.LogDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,15 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-  //private final LogDemoService logDemoService;
-  private final MyLogger myLogger;
+  private final LogDemoService logDemoService;
+  private final ObjectProvider<MyLogger> myLoggerProvider;
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestURL = request.getRequestURL().toString();
+    MyLogger myLogger = myLoggerProvider.getObject();
     myLogger.setRequestURL(requestURL);
     myLogger.log("controller test");
-    //logDemoService.logic("testId");
+    logDemoService.logic("testId");
     return "OK";
   }
 }
